23a019778a21b37d592e78a925b0388e9b67c704
Cris <filipcristimihail@yahoo.com>
2023-11-26 22:20:30 +0100

    #3 Add First Validator for Registration - purpose: Server-Side Validation

--- src/ConsiliumTempus.Application/DependencyInjection.cs
+++ src/ConsiliumTempus.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using ConsiliumTempus.Application.Common.Behavior;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,7 +13,11 @@ public static class DependencyInjection
         services.AddMediatR(cfg =>
             cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
         
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         
         return services;
     }
