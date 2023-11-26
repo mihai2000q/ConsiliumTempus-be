1d0f109724a686ef1df73cd62dfa901b00070112
Cris <filipcristimihail@yahoo.com>
2023-11-26 14:33:49 +0100

    #1 Add Basic Tests

--- tests/Integration/ConsiliumTempus.Application.IntegrationTests/IntegrationTest1.cs
+++ tests/Integration/ConsiliumTempus.Application.IntegrationTests/IntegrationTest1.cs
@@ -1,9 +1,10 @@
 namespace ConsiliumTempus.Application.IntegrationTests;
 
-public class UnitTest1
+public class IntegrationTest1
 {
     [Fact]
     public void Test1()
     {
+        Assert.Equal(2, 2);
     }
 }
