d21616cdd2b260aa0d241ce9aeb2b5faed3e179b
Cris <filipcristimihail@yahoo.com>
2023-11-26 22:15:41 +0100

    #3 Add First Domain Model and its Errors - User (Needs Refactoring)

---
+++ src/ConsiliumTempus.Domain/UserAggregate/User.cs
@@ -0,0 +1,48 @@
+﻿using ConsiliumTempus.Domain.UserAggregate.ValueObjects;
+
+namespace ConsiliumTempus.Domain.UserAggregate;
+
+public class User
+{
+    public UserId Id { get; set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public DateTime CreatedDateTime { get; private set; }
+    public DateTime UpdatedDateTime { get; private set; }
+
+    private User(
+        UserId id, 
+        string firstName, 
+        string lastName, 
+        string email, 
+        string password, 
+        DateTime createdDateTime,
+        DateTime updatedDateTime)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public static User Create(
+        string firstName, 
+        string lastName, 
+        string email, 
+        string password)
+    {
+        return new User(
+            UserId.CreateUnique(),
+            firstName, 
+            lastName,
+            email,
+            password,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+}
---
+++ src/ConsiliumTempus.Domain/UserAggregate/ValueObjects/UserId.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsiliumTempus.Domain.UserAggregate.ValueObjects;
+
+public class UserId
+{
+    public Guid Value { get; set; }
+
+    private UserId(Guid value)
+    {
+        Value = value;
+    }
+    
+    public static UserId CreateUnique()
+    {
+        return new UserId(Guid.NewGuid());
+    }
+}
