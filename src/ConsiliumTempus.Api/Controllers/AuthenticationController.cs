5b71b404bc45f73657eeae7f17f21e438e670094
Cris <filipcristimihail@yahoo.com>
2023-11-26 22:14:56 +0100

    #3 Add Authentication Controller

---
+++ src/ConsiliumTempus.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using ConsiliumTempus.Api.Contracts.Authentication;
+using ConsiliumTempus.Application.Authentication.Commands.Register;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsiliumTempus.Api.Controllers;
+
+[Route("api/auth")]
+[AllowAnonymous]
+public class AuthenticationController : ApiController
+{
+    public AuthenticationController(IMapper mapper, ISender mediator) : base(mapper, mediator)
+    {
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        var command = Mapper.Map<RegisterCommand>(request);
+        var result = await Mediator.Send(command);
+
+        return result.Match(
+            registerResult => Ok(Mapper.Map<RegisterResponse>(registerResult)),
+            Problem
+        );
+    }
+}
