8f61fe3458d6110af85d01f882e1c1261c47675a
Cris <filipcristimihail@yahoo.com>
2023-11-26 22:10:43 +0100

    #3 Add Mapster to the Api Layer + First Mapping Configuration for Authentication

---
+++ src/ConsiliumTempus.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using ConsiliumTempus.Api.Contracts.Authentication;
+using ConsiliumTempus.Application.Authentication.Commands.Register;
+using Mapster;
+
+namespace ConsiliumTempus.Api.Common.Mapping;
+
+public sealed class AuthenticationMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        RegisterMappings(config);
+    }
+
+    private static void RegisterMappings(TypeAdapterConfig config)
+    {
+        config.NewConfig<RegisterRequest, RegisterCommand>();
+
+        config.NewConfig<RegisterResult, RegisterResponse>();
+    }
+}
