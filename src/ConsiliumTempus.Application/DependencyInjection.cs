1182ed101c791b88ff198990d4b911de609cf099
Cris <filipcristimihail@yahoo.com>
2023-11-28 15:30:34 +0100

    #3 Application - Downgrade MediatR to match the expectations of the MediatR DI nuget package

--- src/ConsiliumTempus.Application/DependencyInjection.cs
+++ src/ConsiliumTempus.Application/DependencyInjection.cs
@@ -10,13 +10,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(Assembly.GetExecutingAssembly());
 
         services.AddScoped(
             typeof(IPipelineBehavior<,>),
             typeof(ValidationBehavior<,>));
-        
+
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         
         return services;
