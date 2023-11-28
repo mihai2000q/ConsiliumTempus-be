4784eb36bb332a355e310c0f9b404043d154d2c0
Cris <filipcristimihail@yahoo.com>
2023-11-28 15:27:34 +0100

    #3 Application Layer - New Package - Extensions + New Classes - String Extensions and RegexStore inside the Regex package + Unit Tests

---
+++ src/ConsiliumTempus.Application/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsiliumTempus.Application.Common.Extensions;
+
+public static class StringExtensions
+{
+    private static readonly char[] Numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    
+    public static bool ContainsUppercase(this string str) =>
+        str.Any(char.IsUpper);
+    
+    public static bool ContainsLowercase(this string str) =>
+        str.Any(char.IsLower);
+    
+    public static bool ContainsNumber(this string str) => 
+        str.Any(Numbers.Contains);
+
+    public static bool IsValidEmail(this string str) =>
+        Regex.RegexStore.EmailRegex().IsMatch(str);
+}
---
+++ src/ConsiliumTempus.Application/Common/Regex/RegexStore.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsiliumTempus.Application.Common.Regex;
+
+public static partial class RegexStore
+{
+    [GeneratedRegex("[a-zA-Z0-9._-]+@[a-zA-Z]+\\.+[a-z]+")]
+    public static partial System.Text.RegularExpressions.Regex EmailRegex();
+}
