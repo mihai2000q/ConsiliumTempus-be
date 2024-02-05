b5a54cad5cddb2e12097683e721bf43061f6c50e
Cris <filipcristimihail@yahoo.com>
2024-02-05 10:36:33 +0100

    #25 Domain - New Aggregate: Workspace

---
+++ src/ConsiliumTempus.Domain/Common/Validation/PropertiesValidation.Workspace.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsiliumTempus.Domain.Common.Validation;
+
+public static partial class PropertiesValidation
+{
+    public static class Workspace
+    {
+        public const short NameMaximumLength = 100;
+        public const short DescriptionMaximumLength = 1000;
+    }
+}
