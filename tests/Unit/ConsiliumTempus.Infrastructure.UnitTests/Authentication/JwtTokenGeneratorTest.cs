537d939f2c26159d504d1f363dd8336d2dadcd57
Cris <filipcristimihail@yahoo.com>
2024-02-17 22:26:25 +0100

    #40 Infrastructure - Unit Tests - Replace Moq with NSubstitute

--- tests/Unit/ConsiliumTempus.Infrastructure.UnitTests/Authentication/JwtTokenGeneratorTest.cs
+++ tests/Unit/ConsiliumTempus.Infrastructure.UnitTests/Authentication/JwtTokenGeneratorTest.cs
@@ -1,7 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using ConsiliumTempus.Domain.User.ValueObjects;
 using ConsiliumTempus.Infrastructure.Authentication;
-using FluentAssertions.Extensions;
+using ConsiliumTempus.Infrastructure.UnitTests.TestUtils;
 using Microsoft.Extensions.Options;
 
 namespace ConsiliumTempus.Infrastructure.UnitTests.Authentication;
@@ -23,11 +23,12 @@ public class JwtTokenGeneratorTest
             Issuer = "Issuer"
         };
 
-        var options = new Mock<IOptions<JwtSettings>>();
-        options.SetupGet(o => o.Value)
+        var options = Substitute.For<IOptions<JwtSettings>>();
+        options
+            .Value
             .Returns(_jwtSettings);
 
-        _uut = new JwtTokenGenerator(options.Object);
+        _uut = new JwtTokenGenerator(options);
     }
 
     #endregion
@@ -41,36 +42,14 @@ public class JwtTokenGeneratorTest
                 "FirstyLasty@Example.com",
                 "Password123"),
             Name.Create(
-                "First",
-                "Last"));
-
-        var handler = new JwtSecurityTokenHandler();
-
+                "FirstName",
+                "LastName"));
+        
         // Act
         var outcome = _uut.GenerateToken(user);
 
         // Assert
-        handler.CanReadToken(outcome).Should().BeTrue();
-
-        var outcomeToken = handler.ReadJwtToken(outcome);
-        outcomeToken.Issuer.Should().Be(_jwtSettings.Issuer);
-        outcomeToken.Audiences.Should().HaveCount(1);
-        outcomeToken.Audiences.First().Should().Be(_jwtSettings.Audience);
-        outcomeToken.ValidTo.Should().BeCloseTo(DateTime.UtcNow.AddHours(_jwtSettings.ExpiryHours), 1.Minutes());
-
-        // The 5 below and the 3 from above
-        const int claimsSize = 5 + 3;
-        outcomeToken.Claims.Should().HaveCount(claimsSize);
-        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.Sub).Value
-            .Should().Be(user.Id.Value.ToString());
-        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.Email).Value
-            .Should().Be(user.Credentials.Email);
-        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.GivenName).Value
-            .Should().Be(user.Name.First);
-        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.FamilyName).Value
-            .Should().Be(user.Name.Last);
-        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.Jti).Value
-            .Should().HaveLength(Guid.NewGuid().ToString().Length);
+        Utils.AssertToken(outcome, user, _jwtSettings);
     }
 
     [Fact]
---
+++ tests/Unit/ConsiliumTempus.Infrastructure.UnitTests/TestUtils/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using ConsiliumTempus.Domain.User;
+using ConsiliumTempus.Infrastructure.Authentication;
+using FluentAssertions.Extensions;
+
+namespace ConsiliumTempus.Infrastructure.UnitTests.TestUtils;
+
+internal static class Utils
+{
+    internal static void AssertToken(string token, UserAggregate user, JwtSettings jwtSettings)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        
+        handler.CanReadToken(token).Should().BeTrue();
+
+        var outcomeToken = handler.ReadJwtToken(token);
+        outcomeToken.Issuer.Should().Be(jwtSettings.Issuer);
+        outcomeToken.Audiences.Should().HaveCount(1);
+        outcomeToken.Audiences.First().Should().Be(jwtSettings.Audience);
+        outcomeToken.ValidTo.Should().BeCloseTo(DateTime.UtcNow.AddHours(jwtSettings.ExpiryHours), 1.Minutes());
+
+        // The 5 below and the 3 from above
+        const int claimsSize = 5 + 3;
+        outcomeToken.Claims.Should().HaveCount(claimsSize);
+        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.Sub).Value
+            .Should().Be(user.Id.Value.ToString());
+        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.Email).Value
+            .Should().Be(user.Credentials.Email);
+        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.GivenName).Value
+            .Should().Be(user.Name.First);
+        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.FamilyName).Value
+            .Should().Be(user.Name.Last);
+        outcomeToken.Claims.Single(c => c.Type == JwtRegisteredClaimNames.Jti).Value
+            .Should().HaveLength(Guid.NewGuid().ToString().Length);
+    }
+}
