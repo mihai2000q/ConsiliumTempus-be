fcd7c82e2e31f0b5ad3791fabe128f4061caa1f1
Cris <filipcristimihail@yahoo.com>
2024-02-21 14:26:59 +0100

    #48 Application - Authentication - Login Query Validator Unit Test

---
+++ tests/Unit/ConsiliumTempus.Application.UnitTests/Authentication/Queries/LoginQueryValidatorTest.cs
@@ -0,0 +1,42 @@
+﻿using ConsiliumTempus.Application.Authentication.Queries.Login;
+using ConsiliumTempus.Application.UnitTests.TestData.Authentication.Queries;
+
+namespace ConsiliumTempus.Application.UnitTests.Authentication.Queries;
+
+public class LoginQueryValidatorTest
+{
+    #region Setup
+
+    private readonly LoginQueryValidator _uut = new();
+
+    #endregion
+    
+    [Theory]
+    [ClassData(typeof(LoginQueryValidatorData.GetValidQueries))]
+    public async Task WhenValid_ShouldReturnTrue(LoginQuery query)
+    {
+        // Arrange - parameterized
+        
+        // Act
+        var outcome = await _uut.ValidateAsync(query);
+
+        // Assert
+        outcome.IsValid.Should().BeTrue();
+    }
+    
+    [Theory]
+    [ClassData(typeof(LoginQueryValidatorData.GetInvalidEmailQueries))]
+    [ClassData(typeof(LoginQueryValidatorData.GetInvalidPasswordQueries))]
+    public async Task WhenSingleFieldIsInvalid_ShouldReturnFalse(LoginQuery query, string property, int expectedErrors)
+    {
+        // Arrange - parameterized
+        
+        // Act
+        var outcome = await _uut.ValidateAsync(query);
+
+        // Assert
+        outcome.IsValid.Should().BeFalse();
+        outcome.Errors.Should().HaveCount(expectedErrors);
+        outcome.Errors.Should().AllSatisfy(e => e.PropertyName.Should().Be(property));
+    }
+}
---
+++ tests/Unit/ConsiliumTempus.Application.UnitTests/TestData/Authentication/Queries/LoginQueryValidatorData.cs
@@ -0,0 +1,57 @@
+﻿using ConsiliumTempus.Application.Authentication.Queries.Login;
+using ConsiliumTempus.Common.UnitTests.Authentication;
+using ConsiliumTempus.Domain.Common.Validation;
+
+namespace ConsiliumTempus.Application.UnitTests.TestData.Authentication.Queries;
+
+internal static class LoginQueryValidatorData
+{
+    internal class GetValidQueries : TheoryData<LoginQuery>
+    {
+        public GetValidQueries()
+        {
+            Add(new LoginQuery("MichaelJ@Gmail.com", "Password123"));
+        }
+    }
+    
+    internal class GetInvalidEmailQueries : TheoryData<LoginQuery, string, int>
+    {
+        public GetInvalidEmailQueries()
+        {
+            var query = AuthenticationQueryFactory.CreateLoginQuery(email: "");
+            Add(query, nameof(query.Email), 2);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(email: "random string");
+            Add(query, nameof(query.Email), 1);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(
+                email: new string('a', PropertiesValidation.User.EmailMaximumLength + 1));
+            Add(query, nameof(query.Email), 2);
+        }
+    }
+    
+    internal class GetInvalidPasswordQueries : TheoryData<LoginQuery, string, int>
+    {
+        public GetInvalidPasswordQueries()
+        {
+            var query = AuthenticationQueryFactory.CreateLoginQuery(password: "");
+            Add(query, nameof(query.Password), 5);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(
+                password: new string('a', PropertiesValidation.User.PlainPasswordMaximumLength + 1));
+            Add(query, nameof(query.Password), 3);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(password: "aaa");
+            Add(query, nameof(query.Password), 3);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(password: "Aaaa");
+            Add(query, nameof(query.Password), 2);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(password: "Aaaaaaaaa");
+            Add(query, nameof(query.Password), 1);
+            
+            query = AuthenticationQueryFactory.CreateLoginQuery(password: "aaaaaaaaa123");
+            Add(query, nameof(query.Password), 1);
+        }
+    }
+}
