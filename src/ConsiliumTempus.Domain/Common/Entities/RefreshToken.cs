584444fba80fcc0d5cc0b3722320089db15dc25c
Cris <filipcristimihail@yahoo.com>
2024-03-27 14:23:55 +0100

    #61 Domain & Infrastructure - New Entitiy: Refresh Token + Refresh Token Repository and Configuration

---
+++ src/ConsiliumTempus.Domain/Common/Entities/RefreshToken.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ConsiliumTempus.Domain.Common.Interfaces;
+using ConsiliumTempus.Domain.Common.Models;
+using ConsiliumTempus.Domain.User;
+
+namespace ConsiliumTempus.Domain.Common.Entities;
+
+public sealed class RefreshToken : Entity<Guid>, ITimestamps
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private RefreshToken()
+    {
+    }
+
+    private RefreshToken(
+        Guid token,
+        Guid jwtId,
+        DateTime expiryDate,
+        bool invalidated,
+        long usedTimes,
+        UserAggregate user,
+        DateTime createdDateTime,
+        DateTime updatedDateTime) : base(token)
+    {
+        JwtId = jwtId;
+        ExpiryDate = expiryDate;
+        Invalidated = invalidated;
+        UsedTimes = usedTimes;
+        User = user;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public Guid JwtId { get; init; }
+    public DateTime ExpiryDate { get; init; }
+    public bool Invalidated { get; private set; }
+    public long UsedTimes { get; private set; }
+    public DateTime CreatedDateTime { get; init; }
+    public DateTime UpdatedDateTime { get; private set; }
+    public UserAggregate User { get; init; } = null!;
+
+    public string Value => Id.ToString();
+
+    public static RefreshToken Create(
+        string token,
+        string jwtId,
+        UserAggregate user)
+    {
+        return new RefreshToken(
+            new Guid(token),
+            new Guid(jwtId),
+            DateTime.UtcNow.AddMonths(1),
+            false,
+            0,
+            user,
+            DateTime.UtcNow, 
+            DateTime.UtcNow);
+    }
+
+    public void UpdateUsage()
+    {
+        UsedTimes++;
+        UpdatedDateTime = DateTime.UtcNow;
+    }
+}
---
+++ src/ConsiliumTempus.Infrastructure/Persistence/Repository/RefreshTokenRepository.cs
@@ -0,0 +1,21 @@
+﻿using ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
+using ConsiliumTempus.Domain.Common.Entities;
+using ConsiliumTempus.Infrastructure.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsiliumTempus.Infrastructure.Persistence.Repository;
+
+public sealed class RefreshTokenRepository(ConsiliumTempusDbContext dbContext) : IRefreshTokenRepository
+{
+    public Task<RefreshToken?> Get(string token, CancellationToken cancellationToken)
+    {
+        return dbContext.Set<RefreshToken>()
+            .Include(rt => rt.User)
+            .SingleOrDefaultAsync(rt => rt.Id == new Guid(token), cancellationToken);
+    }
+
+    public async Task Add(RefreshToken refreshToken, CancellationToken cancellationToken)
+    {
+        await dbContext.Set<RefreshToken>().AddAsync(refreshToken, cancellationToken);
+    }
+}
