9252e7fbad029d44e50a538063657da0e45a239e
Cris <filipcristimihail@yahoo.com>
2024-03-27 14:30:51 +0100

    #61 Domain - new Authentication Error: Invalid Tokens

--- src/ConsiliumTempus.Domain/Common/Errors/Errors.Authentication.cs
+++ src/ConsiliumTempus.Domain/Common/Errors/Errors.Authentication.cs
@@ -9,5 +9,9 @@ public static partial class Errors
         public static Error InvalidCredentials => Error.Unauthorized(
             "Authentication.InvalidCredentials",
             "Invalid Credentials");
+        
+        public static Error InvalidTokens => Error.Unauthorized(
+            "Authentication.InvalidTokens",
+            "Invalid Tokens");
     }
 }
