517aa085d3a4628814936d15f351f88b0db1b211
Cris <filipcristimihail@yahoo.com>
2024-04-14 14:36:25 +0200

    #94 Application - Project - new Query: Get Project Query + Unit Testing

---
+++ tests/Unit/ConsiliumTempus.Application.UnitTests/Project/Queries/Get/GetProjectQueryValidatorTest.cs
@@ -0,0 +1,42 @@
+﻿using ConsiliumTempus.Application.Project.Queries.Get;
+using ConsiliumTempus.Common.UnitTests.Project;
+
+namespace ConsiliumTempus.Application.UnitTests.Project.Queries.Get;
+
+public class GetProjectQueryValidatorTest
+{
+    #region Setup
+
+    private readonly GetProjectQueryValidator _uut = new();
+
+    #endregion
+    
+    [Fact]
+    public async Task WhenQueryIsValid_ShouldReturnTrue()
+    {
+        // Arrange
+        var query = ProjectQueryFactory.CreateGetProjectQuery();
+
+        // Act
+        var outcome = await _uut.ValidateAsync(query);
+
+        // Assert
+        outcome.IsValid.Should().BeTrue();
+        outcome.Errors.Should().BeEmpty();
+    }
+    
+    [Fact]
+    public async Task WhenIdIsInvalid_ShouldReturnFalse()
+    {
+        // Arrange
+        var query = ProjectQueryFactory.CreateGetProjectQuery(Guid.Empty);
+
+        // Act
+        var outcome = await _uut.ValidateAsync(query);
+
+        // Assert
+        outcome.IsValid.Should().BeFalse();
+        outcome.Errors.Should().HaveCount(1);
+        outcome.Errors.Should().AllSatisfy(e => e.PropertyName.Should().Be(nameof(query.Id)));
+    }
+}
--- tests/Unit/ConsiliumTempus.Common.UnitTests/Project/ProjectQueryFactory.cs
+++ tests/Unit/ConsiliumTempus.Common.UnitTests/Project/ProjectQueryFactory.cs
@@ -1,10 +1,17 @@
-﻿using ConsiliumTempus.Application.Project.Queries.GetCollectionForUser;
+﻿using ConsiliumTempus.Application.Project.Queries.Get;
+using ConsiliumTempus.Application.Project.Queries.GetCollectionForUser;
 using ConsiliumTempus.Application.Project.Queries.GetCollectionForWorkspace;
 
 namespace ConsiliumTempus.Common.UnitTests.Project;
 
 public static class ProjectQueryFactory
 {
+    public static GetProjectQuery CreateGetProjectQuery(
+        Guid? projectId = null)
+    {
+        return new GetProjectQuery(projectId ?? Guid.NewGuid());
+    }
+    
     public static GetCollectionProjectForUserQuery CreateGetCollectionProjectForUserQuery()
     {
         return new GetCollectionProjectForUserQuery();
