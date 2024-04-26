c6cafbecd6c36187ec6bcc25db52256aa43b8ee1
Cris <filipcristimihail@yahoo.com>
2024-04-26 16:35:08 +0200

    #110 Update Environment Files - Remove connection option from the shell script: Apply Migration To Database

--- scripts/apply-migration-to-database.sh
+++ scripts/apply-migration-to-database.sh
@@ -1,7 +1,5 @@
 DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source ${DIR_SCRIPT}/../.env
-
 tools=$( dotnet tool list --global )
  
 if [[ $tools != *"dotnet-ef"* ]] 
@@ -12,4 +10,4 @@ else
     echo "Dotnet Entity Framework Tool already installed!"
 fi
 
-dotnet ef database update -p ${DIR_SCRIPT}/../src/ConsiliumTempus.Infrastructure -s ${DIR_SCRIPT}/../src/ConsiliumTempus.Api/ --connection "Server=Localhost,$DATABASE_PORT;Database=$DATABASE_NAME;User Id=$DATABASE_USER;Password=$DATABASE_PASSWORD;Encrypt=false"
+dotnet ef database update -p ${DIR_SCRIPT}/../src/ConsiliumTempus.Infrastructure -s ${DIR_SCRIPT}/../src/ConsiliumTempus.Api/
