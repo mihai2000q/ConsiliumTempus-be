526927bcf5802eeee25a312880d6630cad070335
Cris <filipcristimihail@yahoo.com>
2024-04-29 13:58:29 +0200

    #110 Infrastructure - Project Repository - Add Pagination to Get Collection + new method: Get List By User Count + Queryable Extension for Page Based Pagination DOMAIN: Pagination Info

---
+++ src/ConsiliumTempus.Domain/Common/Models/PaginationInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsiliumTempus.Domain.Common.Models;
+
+public sealed record PaginationInfo(
+    int PageSize,
+    int CurrentPage)
+{
+    public int GetTotalPages(int totalCount)
+    {
+        return (int)Math.Floor((decimal)totalCount / PageSize);
+    }
+}
