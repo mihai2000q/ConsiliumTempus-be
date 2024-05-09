6d401fd4015bbbb82519eeab56532e638a72d8b5
Cris <filipcristimihail@yahoo.com>
2024-05-09 11:35:06 +0200

    #101 Application - Project Stage - New Command: Delete

---
+++ src/ConsiliumTempus.Application/Project/Entities/Stage/Commands/Delete/DeleteProjectStageCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
+using ConsiliumTempus.Domain.Common.Errors;
+using ConsiliumTempus.Domain.Project.ValueObjects;
+using ErrorOr;
+using MediatR;
+
+namespace ConsiliumTempus.Application.Project.Entities.Stage.Commands.Delete;
+
+public sealed class DeleteProjectStageCommandHandler(IProjectStageRepository projectStageRepository)
+    : IRequestHandler<DeleteProjectStageCommand, ErrorOr<DeleteProjectStageResult>>
+{
+    public async Task<ErrorOr<DeleteProjectStageResult>> Handle(DeleteProjectStageCommand command, CancellationToken cancellationToken)
+    {
+        var stage = await projectStageRepository.GetWithWorkspace(ProjectStageId.Create(command.Id), cancellationToken);
+        if (stage is null) return Errors.ProjectStage.NotFound;
+        
+        projectStageRepository.Remove(stage);
+        
+        stage.Sprint.Project.RefreshActivity();
+
+        return new DeleteProjectStageResult();
+    }
+}
---
+++ src/ConsiliumTempus.Application/Project/Entities/Stage/Commands/Delete/DeleteProjectStageResult.cs
@@ -0,0 +1,3 @@
+﻿namespace ConsiliumTempus.Application.Project.Entities.Stage.Commands.Delete;
+
+public sealed record DeleteProjectStageResult(string Message = "Project Stage deleted successfully!");
