7557dab081889dd49e0a9380d0bd4ff95c1a5f44
Cris <filipcristimihail@yahoo.com>
2024-05-16 16:18:33 +0200

    #143 Infrastructure - Project Stage Repository - Remove unused functions

--- src/ConsiliumTempus.Application/Common/Interfaces/Persistence/Repository/IProjectStageRepository.cs
+++ src/ConsiliumTempus.Application/Common/Interfaces/Persistence/Repository/IProjectStageRepository.cs
@@ -5,13 +5,5 @@ namespace ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
 
 public interface IProjectStageRepository
 {
-    Task<ProjectStage?> Get(ProjectStageId id, CancellationToken cancellationToken = default);
-
     Task<ProjectStage?> GetWithTasksAndWorkspace(ProjectStageId id, CancellationToken cancellationToken = default);
-
-    Task<ProjectStage?> GetWithWorkspace(ProjectStageId id, CancellationToken cancellationToken = default);
-
-    Task<ProjectStage?> GetWithStagesAndWorkspace(ProjectStageId id, CancellationToken cancellationToken = default);
-
-    Task<List<ProjectStage>> GetListBySprint(ProjectSprintId id, CancellationToken cancellationToken = default);
 }
