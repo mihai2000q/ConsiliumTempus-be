e511167453636ebead13a969f69b3ec36bb34704
Cris <filipcristimihail@yahoo.com>
2024-05-16 10:08:24 +0200

    #143 Domain - Refactor Properties Validation and Errors of Project Sprint and Project Stage

--- src/ConsiliumTempus.Domain/Common/Validation/PropertiesValidator.ProjectSprint.cs
+++ src/ConsiliumTempus.Domain/Common/Validation/PropertiesValidator.ProjectSprint.cs
@@ -2,7 +2,12 @@
 
 public static partial class PropertiesValidation
 {
-    public static class Project
+    public static class ProjectSprint
+    {
+        public const short NameMaximumLength = 100;
+    }
+
+    public static class ProjectStage
     {
         public const short NameMaximumLength = 100;
     }
