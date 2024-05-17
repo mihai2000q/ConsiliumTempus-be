c592547ecc625a091687e1936ea0d75fd9a38d73
Cris <filipcristimihail@yahoo.com>
2024-05-17 13:29:28 +0200

    #151 Api - Integration Testing - Add Missing Delete Validation Tests

---
+++ tests/Integration/ConsiliumTempus.Api.IntegrationTests/Controllers/Workspace/Delete/WorkspaceControllerDelete.ValidationTest.cs
@@ -0,0 +1,41 @@
+﻿using ConsiliumTempus.Api.IntegrationTests.Core;
+using ConsiliumTempus.Api.IntegrationTests.TestCollections;
+using ConsiliumTempus.Api.IntegrationTests.TestData;
+using ConsiliumTempus.Api.IntegrationTests.TestUtils;
+using ConsiliumTempus.Common.IntegrationTests.Workspace;
+
+namespace ConsiliumTempus.Api.IntegrationTests.Controllers.Workspace.Delete;
+
+[Collection(nameof(WorkspaceControllerCollection))]
+public class WorkspaceControllerDeleteValidationTest(WebAppFactory factory)
+    : BaseIntegrationTest(factory, new WorkspaceData())
+{
+    [Fact]
+    public async Task DeleteWorkspace_WhenRequestIsValid_ShouldReturnSuccessResponse()
+    {
+        // Arrange
+        var request = WorkspaceRequestFactory.CreateDeleteWorkspaceRequest(
+            WorkspaceData.Workspaces.First().Id.Value);
+
+        // Act
+        Client.UseCustomToken(WorkspaceData.Users.First());
+        var outcome = await Client.Delete($"api/workspaces/{request.Id}");
+
+        // Assert
+        outcome.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task DeleteWorkspace_WhenRequestIsInvalid_ShouldReturnValidationErrors()
+    {
+        // Arrange
+        var request = WorkspaceRequestFactory.CreateDeleteWorkspaceRequest(
+            id: Guid.Empty);
+
+        // Act
+        var outcome = await Client.Delete($"api/workspaces/{request.Id}");
+
+        // Assert
+        await outcome.ValidateValidationErrors();
+    }
+}
