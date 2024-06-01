4cb25fe6eee78cd569d0861858a3c9f74e3b92e3
Cris <filipcristimihail@yahoo.com>
2024-06-01 17:17:56 +0200

    #165 Domain - Clean WorkspaceRole

--- src/ConsiliumTempus.Domain/Common/Entities/WorkspaceRole.cs
+++ src/ConsiliumTempus.Domain/Common/Entities/WorkspaceRole.cs
@@ -5,28 +5,20 @@ namespace ConsiliumTempus.Domain.Common.Entities;
 
 public sealed class WorkspaceRole : Enumeration<WorkspaceRole>
 {
-    public static readonly WorkspaceRole View = new(1, nameof(View))
-    {
-        Description = "This role can only read data"
-    };
+    public static readonly WorkspaceRole View = new(1, nameof(View), "This role can only read data");
 
-    public static readonly WorkspaceRole Member = new(2, nameof(Member))
-    {
-        Description = "This role can do most of the actions with some limitations"
-    };
+    public static readonly WorkspaceRole Member = new(2, nameof(Member), "This role can do most of the actions with some limitations");
 
-    public static readonly WorkspaceRole Admin = new(3, nameof(Admin))
-    {
-        Description = "This role can do everything"
-    };
+    public static readonly WorkspaceRole Admin = new(3, nameof(Admin), "This role can do everything");
 
     [SuppressMessage("ReSharper", "UnusedMember.Local")]
     private WorkspaceRole()
     {
     }
 
-    private WorkspaceRole(int id, string name) : base(id, name)
+    private WorkspaceRole(int id, string name, string description) : base(id, name)
     {
+        Description = description;
     }
 
     public string Description { get; init; } = string.Empty;
