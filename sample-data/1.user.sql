52407bb4b9e435d997b2da46e4a455fad71a0418
Cris <filipcristimihail@yahoo.com>
2024-06-03 15:40:21 +0200

    #172 Add Sample Data to the Project + Mount on Docker Compose Volume + script

---
+++ sample-data/1.user.sql
@@ -0,0 +1,15 @@
+INSERT INTO [User]
+    (Id, Email, Password, FirstName, LastName, Role, DateOfBirth, CreatedDateTime, UpdatedDateTime)
+VALUES
+    ('10000000-0000-0000-0000-000000000000', 'michael@jordan.com', '$2a$13$q2MPesJ2oXqL7EHnK2XnnujuKkryaO2pNeZE4I7usN/IvsPp6bFle', 
+     'Michael', 'Jordan', 'Pro Basketball Player', '2000-12-12', '2023-11-23 08:10:13', '2023-11-23 09:10:15'),
+    ('20000000-0000-0000-0000-000000000000', 'leo@messi.com', '$2a$13$q2MPesJ2oXqL7EHnK2XnnujuKkryaO2pNeZE4I7usN/IvsPp6bFle', 
+     'Leo', 'Messi', 'Pro Footballer', '2006-09-25', '2023-11-24 09:00:00', '2024-02-24 10:00:00'),
+    ('30000000-0000-0000-0000-000000000000', 'cristiano@ronaldo.com', '$2a$13$q2MPesJ2oXqL7EHnK2XnnujuKkryaO2pNeZE4I7usN/IvsPp6bFle', 
+     'Cristiano', 'Ronaldo', 'The Best Footballer', '2003-06-12', '2023-10-24 09:00:00', '2024-02-24 10:00:00'),
+    ('40000000-0000-0000-0000-000000000000', 'stephen@curry.com', '$2a$13$q2MPesJ2oXqL7EHnK2XnnujuKkryaO2pNeZE4I7usN/IvsPp6bFle', 
+     'Stephen', 'Curry', 'Just Steph', '2006-09-25', '2023-11-12 09:00:00', '2024-02-24 10:00:00'),
+    ('50000000-0000-0000-0000-000000000000', 'lebron@james.com', '$2a$13$q2MPesJ2oXqL7EHnK2XnnujuKkryaO2pNeZE4I7usN/IvsPp6bFle', 
+     'Lebron', 'James', null, '2003-09-25', '2023-10-05 09:00:00', '2024-02-24 10:00:00'),
+    ('60000000-0000-0000-0000-000000000000', 'kevin@durant.com', '$2a$13$q2MPesJ2oXqL7EHnK2XnnujuKkryaO2pNeZE4I7usN/IvsPp6bFle', 
+     'Kevin', 'Durant', 'Basketballer', '1999-01-04', '2023-09-02 23:00:00', '2024-02-24 10:00:00');
