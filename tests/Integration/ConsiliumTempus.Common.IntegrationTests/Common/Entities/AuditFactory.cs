0dc008dd7eaad2baa8cd81905072be5f7dd30ef9
Cris <filipcristimihail@yahoo.com>
2024-06-04 14:55:21 +0200

    #176 Api - Integration Testing - Remove Workspace Role Factory and Add New Audit Factory

---
+++ tests/Integration/ConsiliumTempus.Common.IntegrationTests/Common/Entities/AuditFactory.cs
@@ -0,0 +1,24 @@
+﻿using ConsiliumTempus.Domain.Common.Entities;
+using ConsiliumTempus.Domain.User;
+
+namespace ConsiliumTempus.Common.IntegrationTests.Common.Entities;
+
+public static class AuditFactory
+{
+    public static Audit Create(
+        UserAggregate createdBy,
+        UserAggregate updatedBy,
+        DateTime? createdDateTime = null,
+        DateTime? updatedDateTime = null)
+    {
+        var audit = DomainFactory.GetObjectInstance<Audit>();
+
+        DomainFactory.SetProperty(ref audit, nameof(audit.Id), Guid.NewGuid());
+        DomainFactory.SetProperty(ref audit, nameof(audit.CreatedBy), createdBy);
+        DomainFactory.SetProperty(ref audit, nameof(audit.UpdatedBy), updatedBy);
+        DomainFactory.SetProperty(ref audit, nameof(audit.CreatedDateTime), createdDateTime ?? DateTime.UtcNow);
+        DomainFactory.SetProperty(ref audit, nameof(audit.UpdatedDateTime), updatedDateTime ?? DateTime.UtcNow);
+
+        return audit;
+    }
+}
--- tests/Integration/ConsiliumTempus.Common.IntegrationTests/Common/Entities/WorkspaceRoleFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using ConsiliumTempus.Domain.Common.Entities;
-
-namespace ConsiliumTempus.Common.IntegrationTests.Common.Entities;
-
-public static class WorkspaceRoleFactory
-{
-    public static readonly object[] Roles = [WorkspaceRole.Admin, WorkspaceRole.Member, WorkspaceRole.View];
-}
