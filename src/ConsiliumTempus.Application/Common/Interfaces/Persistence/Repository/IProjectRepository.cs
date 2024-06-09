1ea414eacbb4a31f4befcf7dd68cd7268bc8dc24
Cris <filipcristimihail@yahoo.com>
2024-06-09 22:55:32 +0200

    #180 Infrastructure - Project and Workspace Repositories - Include Favorites

--- src/ConsiliumTempus.Application/Common/Interfaces/Persistence/Repository/IProjectRepository.cs
+++ src/ConsiliumTempus.Application/Common/Interfaces/Persistence/Repository/IProjectRepository.cs
@@ -12,10 +12,6 @@ public interface IProjectRepository
 {
     Task<ProjectAggregate?> Get(ProjectId projectId, CancellationToken cancellationToken = default);
 
-    Task<List<ProjectStatus>> GetStatuses(ProjectId id, CancellationToken cancellationToken = default);
-    
-    Task<int> GetStatusesCount(ProjectId id, CancellationToken cancellationToken = default);
-
     Task<ProjectAggregate?> GetWithWorkspace(ProjectId id, CancellationToken cancellationToken = default);
 
     Task<ProjectAggregate?> GetWithStagesAndWorkspace(ProjectId id, CancellationToken cancellationToken = default);
@@ -33,6 +29,10 @@ public interface IProjectRepository
         WorkspaceId? workspaceId,
         IEnumerable<IFilter<ProjectAggregate>> filters,
         CancellationToken cancellationToken = default);
+
+    Task<List<ProjectStatus>> GetStatuses(ProjectId id, CancellationToken cancellationToken = default);
+
+    Task<int> GetStatusesCount(ProjectId id, CancellationToken cancellationToken = default);
 
     Task Add(ProjectAggregate project, CancellationToken cancellationToken = default);
 
--- src/ConsiliumTempus.Infrastructure/Persistence/Repository/ProjectRepository.cs
+++ src/ConsiliumTempus.Infrastructure/Persistence/Repository/ProjectRepository.cs
@@ -17,34 +17,22 @@ public sealed class ProjectRepository(ConsiliumTempusDbContext dbContext) : IPro
     public async Task<ProjectAggregate?> Get(ProjectId id, CancellationToken cancellationToken = default)
     {
         return await dbContext.Projects
+            .Include(p => p.Favorites)
             .Include(p => p.Statuses.OrderByDescending(s => s.Audit.CreatedDateTime))
             .SingleOrDefaultAsync(p => p.Id == id, cancellationToken);
-    }
-    
-    public async Task<List<ProjectStatus>> GetStatuses(ProjectId id, CancellationToken cancellationToken = default)
-    {
-        return await dbContext.Set<ProjectStatus>()
-            .Where(ps => ps.Project.Id == id)
-            .OrderByDescending(ps => ps.Audit.CreatedDateTime)
-            .ToListAsync(cancellationToken);
-    }
-    
-    public async Task<int> GetStatusesCount(ProjectId id, CancellationToken cancellationToken = default)
-    {
-        return await dbContext.Set<ProjectStatus>()
-            .Where(ps => ps.Project.Id == id)
-            .CountAsync(cancellationToken);
     }
 
     public async Task<ProjectAggregate?> GetWithWorkspace(ProjectId id, CancellationToken cancellationToken = default)
     {
         return await dbContext.Projects
+            .Include(p => p.Favorites)
             .Include(p => p.Statuses)
             .Include(p => p.Workspace)
             .SingleOrDefaultAsync(p => p.Id == id, cancellationToken);
     }
 
-    public async Task<ProjectAggregate?> GetWithStagesAndWorkspace(ProjectId id, CancellationToken cancellationToken = default)
+    public async Task<ProjectAggregate?> GetWithStagesAndWorkspace(ProjectId id,
+        CancellationToken cancellationToken = default)
     {
         return await dbContext.Projects
             .Include(p => p.Workspace)
@@ -66,6 +54,7 @@ public sealed class ProjectRepository(ConsiliumTempusDbContext dbContext) : IPro
         CancellationToken cancellationToken = default)
     {
         return dbContext.Projects
+            .Include(p => p.Favorites)
             .Include(p => p.Statuses.OrderByDescending(s => s.Audit.CreatedDateTime))
             .Where(p => p.Workspace.Memberships.Any(m => m.User.Id == userId))
             .WhereIf(workspaceId is not null, p => p.Workspace.Id == workspaceId!)
@@ -85,6 +74,21 @@ public sealed class ProjectRepository(ConsiliumTempusDbContext dbContext) : IPro
             .Where(p => p.Workspace.Memberships.Any(m => m.User.Id == userId))
             .WhereIf(workspaceId is not null, p => p.Workspace.Id == workspaceId!)
             .ApplyFilters(filters)
+            .CountAsync(cancellationToken);
+    }
+
+    public async Task<List<ProjectStatus>> GetStatuses(ProjectId id, CancellationToken cancellationToken = default)
+    {
+        return await dbContext.Set<ProjectStatus>()
+            .Where(ps => ps.Project.Id == id)
+            .OrderByDescending(ps => ps.Audit.CreatedDateTime)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<int> GetStatusesCount(ProjectId id, CancellationToken cancellationToken = default)
+    {
+        return await dbContext.Set<ProjectStatus>()
+            .Where(ps => ps.Project.Id == id)
             .CountAsync(cancellationToken);
     }
 
