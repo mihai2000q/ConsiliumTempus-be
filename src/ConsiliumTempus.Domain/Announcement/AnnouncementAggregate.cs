e9e187b2aa7a1304a4135f8933ce2c6ada2f3406
Cristina <c.neacsu@lancaster.ac.uk>
2024-07-20 12:26:27 +0200

    #219 Domain - Add new Aggregate: AnnouncementAggregate.cs + PropertiesValidation.Announcement.cs

---
+++ src/ConsiliumTempus.Domain/Announcement/AnnouncementAggregate.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ConsiliumTempus.Domain.Announcement.ValueObjects;
+using ConsiliumTempus.Domain.Common.Entities;
+using ConsiliumTempus.Domain.Common.Models;
+using ConsiliumTempus.Domain.Common.ValueObjects;
+using ConsiliumTempus.Domain.Project;
+using ConsiliumTempus.Domain.User;
+using ConsiliumTempus.Domain.Workspace;
+
+namespace ConsiliumTempus.Domain.Announcement;
+
+public sealed class AnnouncementAggregate : AggregateRoot<AnnouncementId, Guid>
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private AnnouncementAggregate()
+    {
+    }
+
+    private AnnouncementAggregate(
+        AnnouncementId id,
+        Title title,
+        Description description,
+        Audit audit,
+        ProjectAggregate? project,
+        WorkspaceAggregate? workspace) : base(id)
+    {
+        Title = title;
+        Description = description;
+        Audit = audit;
+        Project = project;
+        Workspace = workspace;
+    }
+
+    public Title Title { get; private set; } = default!;
+    public Description Description { get; private set; } = default!;
+    public Audit Audit { get; init; } = default!;
+    public ProjectAggregate? Project { get; init; }
+    public WorkspaceAggregate? Workspace { get; init; }
+
+    public static AnnouncementAggregate Create(
+        Title title,
+        Description description,
+        UserAggregate createdBy,
+        ProjectAggregate project)
+    {
+        return new AnnouncementAggregate(
+            AnnouncementId.CreateUnique(),
+            title,
+            description,
+            Audit.Create(createdBy),
+            project,
+            null);
+    }
+}
