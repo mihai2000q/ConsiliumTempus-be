76490556d309833c57dfa30330e87f34d799250d
Cris <filipcristimihail@yahoo.com>
2024-07-20 23:08:43 +0200

    #226 Domain - New Workspace Entity: Workspace Invitation + Not Found Error + Documentation

--- src/ConsiliumTempus.Domain/Common/Errors/Errors.Workspace.cs
+++ src/ConsiliumTempus.Domain/Common/Errors/Errors.Workspace.cs
@@ -14,4 +14,11 @@ public static partial class Errors
             "Workspace.PersonalWorkspace",
             "Personal Workspaces cannot be deleted, not even by their owners");
     }
+
+    public static class WorkspaceInvitation
+    {
+        public static Error NotFound => Error.NotFound(
+            "WorkspaceInvitation.NotFound",
+            "Workspace Invitation could not be found");
+    }
 }
---
+++ src/ConsiliumTempus.Domain/Workspace/ValueObjects/WorkspaceInvitationId.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ConsiliumTempus.Domain.Common.Models;
+
+namespace ConsiliumTempus.Domain.Workspace.ValueObjects;
+
+public sealed class WorkspaceInvitationId : ValueObject
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    private WorkspaceInvitationId()
+    {
+    }
+
+    private WorkspaceInvitationId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static WorkspaceInvitationId CreateUnique()
+    {
+        return new WorkspaceInvitationId(Guid.NewGuid());
+    }
+
+    public static WorkspaceInvitationId Create(Guid value)
+    {
+        return new WorkspaceInvitationId(value);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
