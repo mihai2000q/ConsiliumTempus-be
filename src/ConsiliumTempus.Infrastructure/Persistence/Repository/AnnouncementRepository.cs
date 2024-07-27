c7f6384805336456e806ca4ceb3e1d80ce2c8468
Cristina <c.neacsu@lancaster.ac.uk>
2024-07-27 19:24:25 +0200

    #219 Infrastructure - new repository: AnnouncementRepository.cs + new db set in db context

---
+++ src/ConsiliumTempus.Infrastructure/Persistence/Repository/AnnouncementRepository.cs
@@ -0,0 +1,12 @@
+﻿using ConsiliumTempus.Domain.Announcement;
+using ConsiliumTempus.Infrastructure.Persistence.Database;
+
+namespace ConsiliumTempus.Infrastructure.Persistence.Repository;
+
+public class AnnouncementRepository(ConsiliumTempusDbContext dbContext)
+{
+    public async Task Add(AnnouncementAggregate announcement, CancellationToken cancellationToken = default)
+    {
+        await dbContext.Announcements.AddAsync(announcement, cancellationToken);
+    }
+}
