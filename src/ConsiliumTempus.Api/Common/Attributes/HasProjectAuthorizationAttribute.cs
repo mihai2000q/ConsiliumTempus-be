648e0867108852df0ed9dc147b11f260b4f4df45
Cris <filipcristimihail@yahoo.com>
2024-07-28 12:09:08 +0200

    #240 Api - New Attribute: Has Project Authorization

---
+++ src/ConsiliumTempus.Api/Common/Attributes/HasProjectAuthorizationAttribute.cs
@@ -0,0 +1,7 @@
+﻿using ConsiliumTempus.Domain.Common.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ConsiliumTempus.Api.Common.Attributes;
+
+public sealed class HasProjectAuthorizationAttribute(ProjectAuthorizationLevel projectAuthorizationLevel) 
+    : AuthorizeAttribute(projectAuthorizationLevel.ToString());
