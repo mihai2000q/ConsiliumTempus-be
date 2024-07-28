deaa97603087c4f8181da39186ae8e2ddab23747
Cristina <c.neacsu@lancaster.ac.uk>
2024-07-28 13:33:48 +0200

    #219 Application - new command: CreateAnnouncementCommand.cs + validator + handler

--- src/ConsiliumTempus.Infrastructure/Persistence/Repository/AnnouncementRepository.cs
+++ src/ConsiliumTempus.Infrastructure/Persistence/Repository/AnnouncementRepository.cs
@@ -1,9 +1,10 @@
-﻿using ConsiliumTempus.Domain.Announcement;
+﻿using ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
+using ConsiliumTempus.Domain.Announcement;
 using ConsiliumTempus.Infrastructure.Persistence.Database;
 
 namespace ConsiliumTempus.Infrastructure.Persistence.Repository;
 
-public class AnnouncementRepository(ConsiliumTempusDbContext dbContext)
+public class AnnouncementRepository(ConsiliumTempusDbContext dbContext) : IAnnouncementRepository
 {
     public async Task Add(AnnouncementAggregate announcement, CancellationToken cancellationToken = default)
     {
