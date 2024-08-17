34a44f157c9a5ed2e5a1548688f86bca1d0bd304
Cris <filipcristimihail@yahoo.com>
2024-08-17 09:40:50 +0200

    #266 Application - New Handler: Workspace Deleted Handler + Add Domain Event to Delete Workspace Command

---
+++ src/ConsiliumTempus.Application/Workspace/Events/WorkspaceDeletedHandler.cs
@@ -0,0 +1,23 @@
+﻿using ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
+using ConsiliumTempus.Domain.Workspace.Events;
+using MediatR;
+
+namespace ConsiliumTempus.Application.Workspace.Events;
+
+public sealed class WorkspaceDeletedHandler(
+    IProjectRepository projectRepository,
+    ICustomFieldSetupRepository customFieldSetupRepository)
+    : INotificationHandler<WorkspaceDeleted>
+{
+    public async Task Handle(WorkspaceDeleted notification, CancellationToken cancellationToken)
+    {
+        var workspace = notification.Workspace;
+
+        var projects = await projectRepository.GetListByWorkspace(workspace.Id, cancellationToken);
+        var customFieldSetups = await customFieldSetupRepository.GetListByWorkspaceOrProjects(
+            workspace.Id,
+            projects,
+            cancellationToken);
+        customFieldSetupRepository.RemoveRange(customFieldSetups);
+    }
+}
---
+++ tests/Unit/ConsiliumTempus.Application.UnitTests/Workspace/Events/WorkspaceDeletedHandlerTest.cs
@@ -0,0 +1,62 @@
+﻿using ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
+using ConsiliumTempus.Application.Workspace.Events;
+using ConsiliumTempus.Common.UnitTests.CustomFieldSetup;
+using ConsiliumTempus.Common.UnitTests.Project;
+using ConsiliumTempus.Common.UnitTests.Workspace;
+using ConsiliumTempus.Domain.CustomFieldSetup;
+using ConsiliumTempus.Domain.Project;
+using ConsiliumTempus.Domain.Workspace.Events;
+using ConsiliumTempus.Domain.Workspace.ValueObjects;
+
+namespace ConsiliumTempus.Application.UnitTests.Workspace.Events;
+
+public class WorkspaceDeletedHandlerTest
+{
+    #region Setup
+
+    private readonly IProjectRepository _projectRepository;
+    private readonly ICustomFieldSetupRepository _customFieldSetupRepository;
+    private readonly WorkspaceDeletedHandler _uut;
+
+    public WorkspaceDeletedHandlerTest()
+    {
+        _projectRepository = Substitute.For<IProjectRepository>();
+        _customFieldSetupRepository = Substitute.For<ICustomFieldSetupRepository>();
+        _uut = new WorkspaceDeletedHandler(_projectRepository, _customFieldSetupRepository);
+    }
+
+    #endregion
+
+    [Fact]
+    public async Task HandleWorksapceDeleted_WhenSuccessful_ShouldRemoveCustomFieldSetupsRelatedToWorkspaceOrProjects()
+    {
+        // Arrange
+        var domainEvent = new WorkspaceDeleted(WorkspaceFactory.Create());
+
+        var projects = ProjectFactory.CreateList();
+        _projectRepository
+            .GetListByWorkspace(Arg.Any<WorkspaceId>())
+            .Returns(projects);
+
+        var customFieldSetups = CustomFieldSetupFactory.CreateList();
+        _customFieldSetupRepository
+            .GetListByWorkspaceOrProjects(Arg.Any<WorkspaceId>(), Arg.Any<List<ProjectAggregate>>())
+            .Returns(customFieldSetups);
+
+        // Act
+        await _uut.Handle(domainEvent, default);
+
+        // Assert
+        await _projectRepository
+            .Received(1)
+            .GetListByWorkspace(Arg.Is<WorkspaceId>(wId => wId == domainEvent.Workspace.Id));
+
+        await _customFieldSetupRepository
+            .GetListByWorkspaceOrProjects(
+                Arg.Is<WorkspaceId>(wId => wId == domainEvent.Workspace.Id),
+                Arg.Is<List<ProjectAggregate>>(p => p == projects));
+        _customFieldSetupRepository
+            .Received(1)
+            .RemoveRange(Arg.Is<List<CustomFieldSetupAggregate>>(cfs => cfs == customFieldSetups));
+    }
+}
