7cdb1d95eeec4e9d88fec28c862a477eac9e1e5b
Cris <filipcristimihail@yahoo.com>
2024-08-17 09:55:20 +0200

    #266 Application - New Domain Event Handler: Project Deleted Handler + Add Domain Event to Delete Project Command

---
+++ src/ConsiliumTempus.Application/Project/Events/ProjectDeletedHandler.cs
@@ -0,0 +1,19 @@
+﻿using ConsiliumTempus.Application.Common.Interfaces.Persistence.Repository;
+using ConsiliumTempus.Domain.Project.Events;
+using MediatR;
+
+namespace ConsiliumTempus.Application.Project.Events;
+
+public sealed class ProjectDeletedHandler(ICustomFieldSetupRepository customFieldSetupRepository)
+    : INotificationHandler<ProjectDeleted>
+{
+    public async Task Handle(ProjectDeleted notification, CancellationToken cancellationToken)
+    {
+        var project = notification.Project;
+
+        var customFieldSetups = await customFieldSetupRepository.GetListByProject(
+            project.Id,
+            cancellationToken);
+        customFieldSetupRepository.RemoveRange(customFieldSetups);
+    }
+}
--- tests/Unit/ConsiliumTempus.Application.UnitTests/Project/Commands/Delete/DeleteProjectCommandHandlerTest.cs
+++ tests/Unit/ConsiliumTempus.Application.UnitTests/Project/Commands/Delete/DeleteProjectCommandHandlerTest.cs
@@ -4,6 +4,7 @@ using ConsiliumTempus.Application.UnitTests.TestUtils;
 using ConsiliumTempus.Common.UnitTests.Project;
 using ConsiliumTempus.Domain.Common.Errors;
 using ConsiliumTempus.Domain.Project;
+using ConsiliumTempus.Domain.Project.Events;
 using ConsiliumTempus.Domain.Project.ValueObjects;
 using NSubstitute.ReturnsExtensions;
 
@@ -48,6 +49,11 @@ public class DeleteProjectCommandHandlerTest
 
         outcome.IsError.Should().BeFalse();
         outcome.Value.Should().Be(new DeleteProjectResult());
+        
+        project.DomainEvents.Should().HaveCount(1);
+        var domainEvent = project.DomainEvents[0];
+        domainEvent.Should().BeOfType<ProjectDeleted>();
+        ((ProjectDeleted)domainEvent).Project.Should().Be(project);
     }
 
     [Fact]
