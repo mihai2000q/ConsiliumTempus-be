8382b8d0613966ec26068455b3127cfe6fc205fc
Cris <filipcristimihail@yahoo.com>
2024-08-17 09:38:36 +0200

    #266 Domain - New Domain Event: Workspace Deleted

---
+++ src/ConsiliumTempus.Domain/Workspace/Events/WorkspaceDeleted.cs
@@ -0,0 +1,5 @@
+﻿using ConsiliumTempus.Domain.Common.Interfaces;
+
+namespace ConsiliumTempus.Domain.Workspace.Events;
+
+public sealed record WorkspaceDeleted(WorkspaceAggregate Workspace) : IDomainEvent;
