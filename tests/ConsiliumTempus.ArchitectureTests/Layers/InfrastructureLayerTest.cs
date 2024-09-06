46ee6fd6d028fa4461383ba8e915f3214a2072bb
Cris <filipcristimihail@yahoo.com>
2024-09-06 09:07:07 +0200

    #266 Infrastructure - Configuration Architecture Changed

--- tests/ConsiliumTempus.ArchitectureTests/Layers/InfrastructureLayerTest.cs
+++ tests/ConsiliumTempus.ArchitectureTests/Layers/InfrastructureLayerTest.cs
@@ -56,23 +56,10 @@ public class InfrastructureLayerTest : BaseArchitectureTest
         ArchRuleDefinition
             .Classes()
             .That()
-            .ResideInNamespace("ConsiliumTempus.Infrastructure.Persistence.Configuration")
-            .Should()
-            .HaveNameEndingWith("Configuration")
-            .Check(Architecture);
-
-        ArchRuleDefinition
-            .Classes()
-            .That()
             .AreAssignableTo(typeof(IEntityTypeConfiguration<>))
             .Should()
             .HaveNameEndingWith("Configuration")
             .Check(Architecture);
-
-        Utils.ShouldHaveSameCount(
-            Architecture,
-            typeof(IEntityTypeConfiguration<>),
-            "ConsiliumTempus.Infrastructure.Persistence.Configuration");
     }
 
     [Fact]
